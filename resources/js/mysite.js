1a4e369bf34ea8e3cfd61b5eed50ac3ca2d40155
ChrisCavs <32932937+ChrisCavs@users.noreply.github.com>
2018-01-03 07:46:24 -0500

    copied files from pc

---
+++ resources/js/mysite.js
@@ -0,0 +1,17 @@
+function openPage(evt, contentName) {
+    // hide all "tabcontent" sections
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // remove "active" class from tablinks
+    tablinks = document.getElementsByClassName("tablinks");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(contentName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
