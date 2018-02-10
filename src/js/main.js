7cea6d542cd2c7d66a2c13ed7743e1c9a7f5a032
Chris <chris211321@gmail.com>
2018-02-10 13:51:54 -0500

    finished re-design

--- src/js/main.js
+++ src/js/main.js
@@ -3,14 +3,30 @@ import {debounce} from './debounce';
 const main = () => {
 
   const fadeLeft = Array.from(document.querySelectorAll('.fadeLeft'));
-  const fadeRight = Array.from(document.querySelectorAll('.fadeReft'));
+  const fadeRight = Array.from(document.querySelectorAll('.fadeRight'));
+
+  fadeRight.forEach(item => {
+    console.log(item.previousSibling);
+    item.previousSibling.addEventListener('transitionend', function() {
+      item.classList.add('active');
+    })
+  });
 
   function checkContent() {
-    
+
+    fadeLeft.forEach(item => {
+
+      let slideInAt = (window.scrollY + window.innerHeight - 50);
+
+      if (slideInAt >= item.offsetTop) {
+        item.classList.add('active');
+      }
+    });
   }
 
-
   window.addEventListener('scroll', debounce(checkContent, 15));
+
+
 }
 
 document.addEventListener('DOMContentLoaded', main)
