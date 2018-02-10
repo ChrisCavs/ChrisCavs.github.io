97b4604d844ef8c5247b5ca6ba3448ef73ce089b
Chris <chris211321@gmail.com>
2018-02-10 13:19:03 -0500

    js fade setup

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,16 @@
+import {debounce} from './debounce';
+
 const main = () => {
-  
+
+  const fadeLeft = Array.from(document.querySelectorAll('.fadeLeft'));
+  const fadeRight = Array.from(document.querySelectorAll('.fadeReft'));
+
+  function checkContent() {
+    
+  }
+
+
+  window.addEventListener('scroll', debounce(checkContent, 15));
 }
 
 document.addEventListener('DOMContentLoaded', main)
