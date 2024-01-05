ee85e073e9fc347c1161b045abb65940a3f9f5b0
Chris C <chrisc@Chriss-MBP.lan>
2024-01-05 16:54:28 -0500

    animate footer text

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,37 +1,47 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { iconMap, translateAnimation, useInViewOnce } from '../Library/utils'
 import { PageTitle, SectionTitle } from '../Library/copy'
 import { ContactLink } from '../Library/link'
-import { iconMap } from '../Library/utils'
 import Spacer from '../Library/spacer'
 
-const Footer = () => (
-  <footer className="contact">
-    <SectionTitle title="Contact" num="04" />
-    <Spacer height="45px" deskHeight="87px" />
-    <PageTitle>Let's work together!</PageTitle>
-    <Spacer height="20px" deskHeight="28px" />
-    <ContactContainer>
-      <ContactLink
-        isFooter
-        text="LinkedIn"
-        Icon={iconMap.Linkedin}
-        href="https://www.linkedin.com/in/chriscavalea"
-      />
-      <Spacer width="25px" deskWidth="39px" />
-      <ContactLink
-        isFooter
-        text="Email"
-        Icon={iconMap.Email}
-        href="mailto:chriscavs94@gmail.com"
-      />
-    </ContactContainer>
-    <Spacer height="69px" deskHeight="79px" />
-  </footer>
-)
+const Footer = () => {
+  const { ref, inView } = useInViewOnce()
+  return (
+    <footer className="contact">
+      <SectionTitle title="Contact" num="04" />
+      <Spacer height="45px" deskHeight="87px" />
+      <FooterPageTitle ref={ref} $inView={inView} $idx={1}>
+        Let's work together!
+      </FooterPageTitle>
+      <Spacer height="20px" deskHeight="28px" />
+      <ContactContainer $inView={inView} $idx={2}>
+        <ContactLink
+          isFooter
+          text="LinkedIn"
+          Icon={iconMap.Linkedin}
+          href="https://www.linkedin.com/in/chriscavalea"
+        />
+        <Spacer width="25px" deskWidth="39px" />
+        <ContactLink
+          isFooter
+          text="Email"
+          Icon={iconMap.Email}
+          href="mailto:chriscavs94@gmail.com"
+        />
+      </ContactContainer>
+      <Spacer height="69px" deskHeight="79px" />
+    </footer>
+  )
+}
+
+const FooterPageTitle = styled(PageTitle)`
+  ${translateAnimation()}
+`
 
 const ContactContainer = styled.div`
+  ${translateAnimation()}
   display: flex;
 `
 
